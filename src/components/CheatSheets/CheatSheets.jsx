493e4cced0f0c5bb3ace45745dee3374c72a338b
designs-by-kate <kate_diet@hotmail.com>
2024-03-21 13:45:59 +0000

    moved pdf file to assets

--- src/components/CheatSheets/CheatSheets.jsx
+++ src/components/CheatSheets/CheatSheets.jsx
@@ -6,7 +6,7 @@ const CheatSheets = () => {
       <h1>Cheat Sheets</h1>
       <h3>Download CheatSheet:</h3>
       <a href="./public/html.pdf" download>HTML</a>
-      <a href="./public/css.pdf" download>CSS</a>
+      <a href="../../assets/css.pdf" download>CSS</a>
       <a href="./public/javascript.pdf" download>JS</a>
     </div>
   );
