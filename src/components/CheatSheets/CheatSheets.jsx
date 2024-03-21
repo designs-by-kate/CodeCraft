2d058aa66842e32ee83eb0fd4ea73fdc6b93900f
designs-by-kate <kate_diet@hotmail.com>
2024-03-21 13:59:26 +0000

    edited <a href=html.pdf download type=application/pdf>HTML</a>

--- src/components/CheatSheets/CheatSheets.jsx
+++ src/components/CheatSheets/CheatSheets.jsx
@@ -5,9 +5,9 @@ const CheatSheets = () => {
     <div className="cs-container">
       <h1>Cheat Sheets</h1>
       <h3>Download CheatSheet:</h3>
-      <a href="html.pdf" download>HTML</a>
-      <a href="css.pdf" download>CSS</a>
-      <a href="javascript.pdf" download>JS</a>
+      <a href="html.pdf" download type="application/pdf">HTML</a>
+      <a href="css.pdf" download type="application/pdf">CSS</a>
+      <a href="javascript.pdf" download type="application/pdf">JS</a>
     </div>
   );
 };
