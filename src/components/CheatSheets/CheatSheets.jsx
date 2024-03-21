281dc34880bb73ce4cc3fb831a51647e5bcb0d3e
designs-by-kate <kate_diet@hotmail.com>
2024-03-21 13:54:29 +0000

    moved pdf file back to public folder

--- src/components/CheatSheets/CheatSheets.jsx
+++ src/components/CheatSheets/CheatSheets.jsx
@@ -5,9 +5,9 @@ const CheatSheets = () => {
     <div className="cs-container">
       <h1>Cheat Sheets</h1>
       <h3>Download CheatSheet:</h3>
-      <a href="../../assets/html.pdf" download>HTML</a>
-      <a href="../../assets/css.pdf" download>CSS</a>
-      <a href="../../assets/javascript.pdf" download>JS</a>
+      <a href="html.pdf" download>HTML</a>
+      <a href="css.pdf" download>CSS</a>
+      <a href="javascript.pdf" download>JS</a>
     </div>
   );
 };
